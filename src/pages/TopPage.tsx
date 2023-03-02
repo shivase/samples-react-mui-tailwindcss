5e5791c74c1baeb4abf03f566c260d992842c782
shivase <shivase@ymail.ne.jp>
2023-03-02 11:20:07 +0900

    style: fix

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,78 +1,46 @@
-import { Box, Button } from '@mui/material';
+import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export const TopPageApp = () => {
   return (
-    <Box className="mt-[50px] flex flex-col items-center">
-      <Box className="mb-[50px]">
-        <Box className="mb-[24px]">Material-UI and React Learn by building projects</Box>
-        <Box className="flex flex-col gap-y-3">
-          <Button variant="contained" color="primary" component={Link} to="/creative">
+    <div className="mt-16 flex flex-col items-center space-y-10">
+      <div className="flex flex-col space-y-4">
+        <div className="mb-2">Material-UI and React Learn by building projects</div>
+        <div className="flex flex-wrap items-center justify-center gap-5">
+          <Button variant="outlined" component={Link} to="/creative">
             Creative Agency
           </Button>
-        </Box>
-      </Box>
-      <Box>
-        <Box className="mb-2">Tailwind CSS From Scratch Learn By Building Projects</Box>
-        <Box className="flex flex-col gap-y-3">
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/sandbox">
+        </div>
+      </div>
+      <div className="flex flex-col space-y-4">
+        <div className="mb-2 text-center">Tailwind CSS From Scratch Learn By Building Projects</div>
+        <div className="flex flex-wrap items-center justify-center gap-5">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/sandbox">
             TailwindCSS Sandbox
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/mini-projects">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/mini-projects">
             Mini Projects
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/clipboard">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/clipboard">
             Clipboard Website
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/loopstudios">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/loopstudios">
             Loopstudios Website
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/shortly">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/shortly">
             Shortly Website
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/testimonial">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/testimonial">
             Testimonial Grid Project
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/fylo">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/fylo">
             Fylo Website with Color Picker
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            component={Link}
-            to="/tailwindcss-from-scratch/bookmark">
+          <Button variant="outlined" component={Link} to="/tailwindcss-from-scratch/bookmark">
             Bookmark Manager Website
           </Button>
-        </Box>
-      </Box>
-    </Box>
+        </div>
+      </div>
+    </div>
   );
 };
